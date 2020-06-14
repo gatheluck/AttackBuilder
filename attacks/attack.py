2a7bc9223ae6a85ed0eb8a36975887a973706e6f
gatheluck <gatheluck+tech@gmail.com>
2020-06-14 13:57:13 +0900

    add attack.py

---
+++ attacks/attack.py
@@ -0,0 +1,74 @@
+import os
+import sys
+
+base = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../')
+sys.path.append(base)
+
+from attacks.util import Normalizer, Denormalizer
+
+import torch
+
+
+class PixelModel(torch.nn.Module):
+    """
+
+    """
+    def __init__(self, model, input_size, mean, std):
+        super().__init__()
+        self.model = model
+        self.normalizer = Normalizer(input_size, mean, std)
+
+    def forward(self, x):
+        """
+        [0, 255] -> [0, 1]
+        """
+        x = self.normalizer(x)
+        return self.model(x)
+
+
+class AttackWrapper(torch.nn.Module):
+    def __init__(self, **kwargs):
+        """
+        Args
+        - input_size
+        - mean
+        - std
+        """
+        required_keys = set('input_size mean std'.split())
+        parsed_args = self._parse_args(required_keys, kwargs)
+
+        for k, v in parsed_args.items():
+            setattr(self, k, v)
+
+        self.normalizer = Normalizer(self.input_size, self.mean, self.std)
+        self.denormalizer = Denormalizer(self.input_size, self.mean, self.std)
+
+    def forward(self, model, x, *args, **kwargs):
+        was_training = model.was_training
+        pixel_model = PixelModel(model, self.input_size, self.mean, self.std)
+        pixel_model.eval()
+        # forward input to  pixel space
+        pixel_x = self.denormalizer(x.detach())
+        pixel_return = self._forward(pixel_model, pixel_x, *args, **kwargs)
+        if was_training:
+            pixel_model.train()
+
+        return self.normalizer(pixel_return)
+
+    @classmethod
+    def _parse_args(cls, required_keys: set, input_args: dict) -> dict:
+        """
+        parse input args
+        Args
+        - required_keys (set) : set of required keys for input_args
+        - input_args (dict)   : dict of input arugments
+        """
+        parsed_args = dict()
+
+        for k in required_keys:
+            if k not in input_args.keys():
+                raise ValueError('initial args are invalid.')
+            else:
+                parsed_args[k] = input_args[k]
+
+        return parsed_args
